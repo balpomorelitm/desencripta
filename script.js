faf528867ff1952ec195a45ca6430c9767977b9a
balpomorelitm <pablotorrado@gmail.com>
2025-07-09 11:00:08 +0800

    Keep new game button visible

--- script.js
+++ script.js
@@ -89,7 +89,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (newGameButton) {
         newGameButton.addEventListener('click', () => {
             startNewGame();
-            newGameButton.style.display = 'none';
         });
     }
 
